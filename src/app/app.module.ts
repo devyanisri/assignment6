78263e181cf2dd87b1835a1d9a11d08d3dd7560b
Srivastava <dsrivastav23@dxc.com>
2019-10-09 00:22:50 +0530

    some text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavComponent } from './nav/nav.component';
+import { CardComponent } from './card/card.component';
+import { CartComponent } from './cart/cart.component';
+import { AddressComponent } from './address/address.component';
+import { BillComponent } from './bill/bill.component';
+import { BookinfoComponent } from './bookinfo/bookinfo.component';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CardComponent,
+    CartComponent,
+    AddressComponent,
+    BillComponent,
+    BookinfoComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
